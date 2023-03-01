a4c9afe27ccf727b3d6fd05d004f3ce261074b4a
Peter <peter@Peters-Air.attlocal.net>
2023-03-01 17:58:25 -0600

    test1

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
-let content = document.querySelector('body');
-content.style.backgroundColor = 'red';
+//use queryselectorall to grab node with class yt-core-image -> returns array of nodes
+    // .src property gets the image source
+    // have src array
+const imgList = document.querySelectorAll('.yt-core-image')
+const srcList = imgList.map(img => img.src)
+
+// iterate through node list
+    // replace src with a random element in src array
+// const scrambledImgs = imgList.map(img => {
+//     const randomIdx = Math.floor(Math.random() * imgList.length);
+//     img.src = srcList[randomIdx];
+// })
+
+for (let i = 0; i < imgList.length; i++) {
+    const randomIdx = Math.floor(Math.random() * imgList.length);
+    imgList[i].src = srcList[randomIdx];
+}
